3bfb29cff7e55bda57d49e96e422d9447bb8ee5f
Helena Pedro <mbeua94@gmail.com>
2025-03-15 15:18:38 +0100

    Initial app boilerplate

---
+++ app.py
@@ -0,0 +1,28 @@
+from dash import Dash, dcc, callback, Output, Input, no_update
+import dash_ag_grid as dag
+import plotly.express as px
+import pandas as pd
+
+# df_pledges = pd.read_json("https://storage.googleapis.com/plotly-app-challenge/one-for-the-world-pledges.json")
+df_payments = pd.read_json("https://storage.googleapis.com/plotly-app-challenge/one-for-the-world-payments.json")
+
+
+fig = px.histogram(df_payments, x='payment_platform')
+
+grid = dag.AgGrid(
+    id='payments-table',
+    rowData=df_payments.to_dict("records"),
+    columnDefs=[{"field": i, 'filter': True, 'sortable': True} for i in df_payments.columns],
+    dashGridOptions={"pagination": True}
+)
+
+app = Dash()
+app.layout = [
+    dcc.Markdown("# One For The World - Getting Started"),
+    grid,
+    dcc.Graph(id='platform-fig', figure=fig)
+]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
